e2fbad6b6bf3a43d560cc9a873f0932628c74fc9
alekse <solodko2001@mail.ru>
2024-10-26 16:38:00 +0300

    init: init project

---
+++ my-todo-app/src/router.tsx
@@ -0,0 +1,20 @@
+// src/router.tsx
+import { createBrowserRouter } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
+
+export default router;
